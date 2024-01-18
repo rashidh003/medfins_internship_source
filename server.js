53353d59f90356decdb2c7d67514e52500bde1cd
rashidh003 <get.rashidh@gmail.com>
2024-01-18 23:38:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index");
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`the server is running on port ${port}`);
+})
